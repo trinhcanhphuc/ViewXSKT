db01945d1971063e50713a1e0417e7bcf5a4cffa
trinhcanhphuc <trinhcanhphuc@outlook.com>
2017-04-28 21:06:25 +0700

    Pass data act -> frag
    Pass data from activity to fragment demo

--- app/src/main/java/com/example/trinhcanhphuc/viewxskt/InforDateTD.java
+++ app/src/main/java/com/example/trinhcanhphuc/viewxskt/InforDateTD.java
@@ -1,22 +1,26 @@
 package com.example.trinhcanhphuc.viewxskt;
 
-import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-public class InforDateTD extends Activity {
+
+public class InforDateTD extends AppCompatActivity {
 
     TextView txtNum;
     int num;
+    private static FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infor_date_td);
+
+        fragmentManager = getSupportFragmentManager();
 
         txtNum = (TextView) findViewById(R.id.txtNum);
 
@@ -25,26 +29,14 @@ public class InforDateTD extends Activity {
 
         txtNum.setText(String.valueOf(b.getInt("num")));
         num = b.getInt("num");
-    }
 
-    public void FragmentOneClick(View view) {
-        Fragment myfragment;
-        myfragment = new FragmentOne();
 
-        FragmentManager fm = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fm.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_switch, myfragment);
-        fragmentTransaction.commit();
-    }
+        Fragment argumentFragment = new ListNumTTFragment();//Get Fragment Instance
+        Bundle bundle = new Bundle();
+        bundle.putInt("number", num);
+        argumentFragment.setArguments(bundle);//Finally set argument bundle to fragment
 
-    public void FragmentTwoClick(View view) {
-        Fragment myfragment;
-        myfragment = new FragmentTwo();
-
-        FragmentManager fm = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fm.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_switch, myfragment);
-        fragmentTransaction.commit();
+        fragmentManager.beginTransaction().replace(R.id.fragmentContainer, argumentFragment).commit();
 
     }
 }
